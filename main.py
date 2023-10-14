522459683a07c536fb53fd3d1b161a5ffd48f22f
SrijanWMXJ <126784572+SrijanWMXJ@users.noreply.github.com>
2023-10-14 19:41:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,8 +43,10 @@ if file is not None:
     st.image(image, use_column_width=True)
 
     # classify image
-    class_name, conf_score = classify(image, model, class_names)
+    # class_name, conf_score = classify(image, model, class_names)
+    class_name = classify(image, model, class_names)
 
+    
     # write classification
     st.write("## {}".format(class_name))
-    st.write("### score: {}%".format(int(conf_score * 1000) / 10))
+    # st.write("### score: {}%".format(int(conf_score * 1000) / 10))
